a5c9eee8fb8851746338a18c3e24052f0b4a7a02
KelvinAraujo <kelven7araujo@gmail.com>
2022-02-12 10:39:37 -0300

    Update desafio 011

--- desafio 011.py
+++ desafio 011.py
@@ -1,6 +1,7 @@
 l = float(input('\033[33m largura da parede:'))
 a = float(input('\033[35m altura da parede:'))
 are = l * a
-print('A sua parede tem dimensão de {}m x {}m e a sua area é de {}m²'.format(l,a,are))
+print('A sua parede tem dimensão de {}m x {}m e a sua area é de {}m²'.format(l, a, are))
 t = are / 2
 print('Para pintar esta parede você precisa de {:.2f}L de tinta'.format(t))
+Testando...
